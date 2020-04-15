c455e73569ac7bbb068bac12259f0f3d51e16330
Penelope LI <penelopeli@Penelopes-MacBook-Pro.local>
2020-04-15 10:44:01 -0600

    add page 2

---
+++ src/app/setup/setup.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-setup',
+  templateUrl: './setup.component.html',
+  styleUrls: ['./setup.component.scss']
+})
+export class SetupComponent implements OnInit {
+  categories: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
