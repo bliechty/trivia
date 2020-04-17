c95216a9055203da84c6f8c7cdfb5cd667c1fd93
Brady Liechty <berrybloxinator@gmail.com>
2020-04-16 20:47:05 -0600

    Removed firebase information from environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyAayqPi75_IIAt0TeYOF1s6WDjQXsm_u6U',
-    authDomain: 'trivia-43e20.firebaseapp.com',
-    databaseURL: 'https://trivia-43e20.firebaseio.com',
-    projectId: 'trivia-43e20',
-    storageBucket: 'trivia-43e20.appspot.com',
-    messagingSenderId: '766024110773',
-    appId: '1:766024110773:web:3b5439860006b47a3424fc'
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyAayqPi75_IIAt0TeYOF1s6WDjQXsm_u6U',
-    authDomain: 'trivia-43e20.firebaseapp.com',
-    databaseURL: 'https://trivia-43e20.firebaseio.com',
-    projectId: 'trivia-43e20',
-    storageBucket: 'trivia-43e20.appspot.com',
-    messagingSenderId: '766024110773',
-    appId: '1:766024110773:web:3b5439860006b47a3424fc'
-  }
+  production: false
 };
 
 /*
