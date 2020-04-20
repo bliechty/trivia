fe8ee0932d332ea6128a81a6190cc7ac27c75bc1
Brady Liechty <berrybloxinator@gmail.com>
2020-04-20 13:02:37 -0600

    removed firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAayqPi75_IIAt0TeYOF1s6WDjQXsm_u6U',
-    authDomain: 'trivia-43e20.firebaseapp.com',
-    databaseURL: 'https://trivia-43e20.firebaseio.com',
-    projectId: 'trivia-43e20',
-    storageBucket: 'trivia-43e20.appspot.com',
-    messagingSenderId: '766024110773',
-    appId: '1:766024110773:web:3b5439860006b47a3424fc'
-  }
+  firebase: {}
 };
 
 /*
